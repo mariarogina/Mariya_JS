f747e9d9199fb6310f73799a472859306ba63020
Mariia <whitesheepa@gmail.com>
2021-06-14 17:00:36 +0300

    123

--- ux-demo/src/App.js
+++ ux-demo/src/App.js
@@ -4,8 +4,8 @@ import Popup from './Components/Popup';
 import React, {useEffect, useState} from "react"
 
 const countriesList = [
-  {name: "Russia", capital: "Moscow", currency: "RUB", language : "russian", img:"https://www.elitetraveler.com/wp-content/uploads/2012/12/RAI5-462x346.jpg"},
-  {name: "Finland", capital: "Helsinki", currency: "EUR", language : "Finnish", img: "https://media-cdn.tripadvisor.com/media/photo-s/13/d7/93/fe/helsinki-by-night.jpg"},
+  {name: "RUSSIA", capital: "Moscow", currency: "RUB", language : "russian", img:"https://www.elitetraveler.com/wp-content/uploads/2012/12/RAI5-462x346.jpg"},
+  {name: "FINLAND", capital: "Helsinki", currency: "EUR", language : "Finnish", img: "https://media-cdn.tripadvisor.com/media/photo-s/13/d7/93/fe/helsinki-by-night.jpg"},
   {name: "Great Britain", capital: "London", currency: "GBP", language : "English",img:"https://www.sftrips.com/wp-content/uploads/2016/03/z8888.jpg"}
 ]
 
