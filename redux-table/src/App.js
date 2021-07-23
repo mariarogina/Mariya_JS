9b436df1ca76d4407b12f7391acbc1ea0709a735
Mariia <whitesheepa@gmail.com>
2021-07-23 20:47:14 +0300

    commit

--- redux-table/src/App.js
+++ redux-table/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  console.log(props.store)
   return (
     <div className="App">
       <header className="App-header">
