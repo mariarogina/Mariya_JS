85ce3dba8f34874781f3c437ca87a57f54b2f4da
greyewi1 <greyewi@gmail.com>
2021-09-13 16:18:39 +0500

    fix ac

--- online-store/src/reducers/cart.js
+++ online-store/src/reducers/cart.js
@@ -1,14 +1,14 @@
 import {
-  SET_CART_TOTAL,
-  GET_CART_ITEMS_SUCCESS,
-  FETCH_ORDER_REQUEST,
+  CHANGE_FORM_FIELD,
   FETCH_ORDER_FAILURE,
+  FETCH_ORDER_REQUEST,
   FETCH_ORDER_SUCCESS,
-  CHANGE_FORM_FIELD
+  GET_CART_ITEMS_SUCCESS,
+  SET_CART_TOTAL
 } from '../actions/actionTypes'
 
-import urls from '../constants';
-import {take, put} from 'redux-saga/effects'
+import urls from '../constants'
+import {put, take, select} from 'redux-saga/effects'
 
 const initialState = {
   cartItems: null,
@@ -17,62 +17,62 @@ const initialState = {
   error: null,
   owner: {phone: '', address: ''},
   success: false
-};
+}
 
 console.log(initialState)
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
 
     case GET_CART_ITEMS_SUCCESS:
-      const {cartItems} = action.payload;
+      const {cartItems} = action.payload
       return {
         ...state,
         cartItems,
         success: false
-      };
+      }
     case CHANGE_FORM_FIELD:
-        const { name, value } = action.payload;
-        const {owner} = state;
-        return {
-          ...state,
-          owner: {
-            ...owner,
-            [name]: value,
-          }
-        };
+      const {name, value} = action.payload
+      const {owner} = state
+      return {
+        ...state,
+        owner: {
+          ...owner,
+          [name]: value,
+        }
+      }
     case FETCH_ORDER_REQUEST:
-    return {
-      ...state,
-      loading: true,
-      error: null
-    };
+      return {
+        ...state,
+        loading: true,
+        error: null
+      }
     case FETCH_ORDER_FAILURE:
-    const {error} = action.payload;
-    return {
-      ...state,
-      error,
-      loading: false
-    };
+      const {error} = action.payload
+      return {
+        ...state,
+        error,
+        loading: false
+      }
     case FETCH_ORDER_SUCCESS:
-    return {
-      ...initialState,
-      success: true
-    };
+      return {
+        ...initialState,
+        success: true
+      }
     case SET_CART_TOTAL:
-        const {total} = action.payload;
+      const {total} = action.payload
       return {
         ...state,
         totalSum: total
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
 
 // export const fetchOrder = () => async (dispatch, getState) => {
 //   const {cart: {cartItems, owner}} = getState();
 //   dispatch(fetchOrderRequest());
-  
+
 //   const items = [];
 //   cartItems.forEach(item => {
 //     items.push({
@@ -89,54 +89,55 @@ export default function cartReducer(state = initialState, action) {
 //     },
 //     items: items
 //   }
-  
 
+const cartSelector = state => state.cart
 
+export const fetchOrderSaga = function* () {
+  while (true) {
+    yield take(FETCH_ORDER_REQUEST)
+    //const {cartItems, owner} = yield select(cartSelector)
+    const {cart : {cartItems, owner}} = yield select(state => state)
 
-// export const fetchOrderSaga = function* () {
-//   while(true) {
-//     yield take(FETCH_ORDER_REQUEST)
-//     const {cart: {cartItems, owner}} = getState();
-      
-//       const items = [];
-//       cartItems.forEach(item => {
-//         items.push({
-//           id: item.id,
-//           price: item.price,
-//           count: item.quantity
-//         })
-//       });
-    
-//       const body = {
-//         owner: {
-//           phone: owner.phone,
-//           address: owner.address,
-//         },
-//         items: items
-//       }
-      
-//       try {
-//             const response = yield fetch(`${urls.order}`, {
-//               method: 'POST',
-//               headers: {
-//                 'Content-Type': 'application/json;charset=utf-8'
-//               },
-//               body: JSON.stringify(body)
-//             });
-        
-//             if (!response.ok) {
-//               throw new Error(response.statusText);
-//             }
-//             localStorage.clear();
-        
-//             yield put({
-//           type: FETCH_ORDER_SUCCESS,
-//           payload: items
-//         })
-//           } catch (error) {
-//               yield put({
-//                 type: FETCH_ORDER_FAILURE,
-//                 payload: error.message
-//               })
-//             }
-// }}
+    const items = []
+    cartItems.forEach(item => {
+      items.push({
+        id: item.id,
+        price: item.price,
+        count: item.quantity
+      })
+    })
+
+    const body = {
+      owner: {
+        phone: owner.phone,
+        address: owner.address,
+      },
+      items: items
+    }
+
+    try {
+      const response = yield fetch(`${urls.order}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(body)
+      })
+
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      localStorage.clear()
+
+      yield put({
+        type: FETCH_ORDER_SUCCESS,
+        payload: items
+      })
+    } catch (error) {
+      yield put({
+        type: FETCH_ORDER_FAILURE,
+        payload: error.message
+      })
+    }
+  }
+}
